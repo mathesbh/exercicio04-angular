f518f1863bb920896825de9a4ef49d93159c5b95
mathesbh <mathesbh@gmail.com>
2021-05-12 22:37:53 -0300

    Modifica componente para receber movies do service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,64 +1,18 @@
-import { Component } from '@angular/core';
+import { MoviesService } from './services/movies.service';
+import { Component, OnInit } from '@angular/core';
+import { IMovieItem } from './models/IMovieItem';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Movies App';
-  movies = [
-    {
-      id: 1,
-      title: 'Mortal Kombat',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
-    },
-    {
-      id: 2,
-      title: 'Godzilla vs. Kong',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/klAIFewuqcyEmH1SMtR2XbMyqzM.jpg',
-    },
-    {
-      id: 3,
-      title: 'Liga da Justiça de Zack Snyder',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/ArWn6gCi61b3b3hclD2L0LOk66k.jpg',
-    },
-    {
-      id: 4,
-      title: 'Sociedade da Justiça: 2ª Guerra Mundial',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/haFimehtZoP6rSz53kEqXSKianS.jpg',
-    },
-    {
-      id: 5,
-      title: 'Mulher-Maravilha 1984',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/qDA95ebiy3W3m8hTRB3xZNZVVBM.jpg',
-    },
-    {
-      id: 6,
-      title: 'Crocodilos: A Morte Te Espera',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/9I2ATBaOfwXo23vSBfEAQ3uZhGV.jpg',
-    },
-    {
-      id: 7,
-      title: 'Os Croods 2: Uma Nova Era',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/vRTScutueW9gLF3dSo2Za7o0v7n.jpg',
-    },
-    {
-      id: 8,
-      title: 'Passageiro Acidental',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/n91IdvTOxogvs08HN0mYK7nY5T7.jpg',
-    },
-    {
-      id: 9,
-      title: 'Rise of the Mummy',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/tmghT8HaddVIS9hEXIOI9GuDchi.jpg',
-    },
-    {
-      id: 10,
-      title: 'Jiu Jitsu',
-      imgUrl: 'https://www.themoviedb.org/t/p/w220_and_h330_face/eLT8Cu357VOwBVTitkmlDEg32Fs.jpg',
-    },
-  ];
+
+  movies: Array<IMovieItem> = [];
+
+  constructor(private MoviesService: MoviesService){}
 
   listedMovies = [];
 
@@ -69,5 +23,11 @@ export class AppComponent {
     } else {
       this.listedMovies = this.movies;
     }
+  }
+
+  ngOnInit(): void {
+    this.MoviesService.getMovies().subscribe(data => {
+      this.movies = data.results;
+    })
   }
 }
