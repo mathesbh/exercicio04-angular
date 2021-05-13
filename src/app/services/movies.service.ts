7e4a7808a2a2a44cb8525de2694272b39e3d568b
mathesbh <mathesbh@gmail.com>
2021-05-12 21:54:09 -0300

    Adiciona metodo getMovies api

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { IMovieApiResult } from '../models/IMovieApiResult';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MoviesService {
+  private api_key = 'd416af5d4faee64e25ab001d87aab5c3';
+  private base_url = 'https://api.themoviedb.org/3';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getMovies(): Observable<IMovieApiResult> {
+    return this.http.get<IMovieApiResult>(`${this.base_url}/movie/popular?api_key=${this.api_key}`);
+  }
 }
