f47e5f1284ba89b9baa64d7d63731a51acd07e1d
mathesbh <mathesbh@gmail.com>
2021-05-12 22:15:33 -0300

    Importa httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
@@ -13,7 +14,7 @@ import { MovieComponent } from './components/movie/movie.component';
     MovieComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
